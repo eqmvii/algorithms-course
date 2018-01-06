7abe0822ee2063c59f6bbba7012b3a1de1aeae6c
eqmvii <coelomate@gmail.com>
2018-01-06 12:09:48 -0500

    Problem set #3 first commit / boiler plate

---
+++ PatternRec/BruteCollinearPoints.java
@@ -0,0 +1,14 @@
+// BruteCollinearPoints.java
+// Checks 4 points to see if they lie on the same line segment
+// For Algorithms, Part I course
+// Specifications from: http://coursera.cs.princeton.edu/algs4/assignments/collinear.html
+// by Eric Mancini
+
+// Corner case info: No samples will have 5 or more collinear points
+// Performance requirement: n^4 at worst runtime, n plus line segments returned space
+
+public class BruteCollinearPoints {
+   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
+   public           int numberOfSegments()        // the number of line segments
+   public LineSegment[] segments()                // the line segments
+}
---
+++ PatternRec/FastCollinearPoints.java
@@ -0,0 +1,14 @@
+// FastCollinearPoints.java
+// Checks 4 points to see if they lie on the same line segment
+// For Algorithms, Part I course
+// Specifications from: http://coursera.cs.princeton.edu/algs4/assignments/collinear.html
+// by Eric Mancini
+
+// Corner case info: Should work even with 5 or more collinear points
+// Performance requirement: n^2 log(n) at worst runtime, n plus line segments returned space
+
+public class FastCollinearPoints {
+   public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
+   public           int numberOfSegments()        // the number of line segments
+   public LineSegment[] segments()                // the line segments
+}
